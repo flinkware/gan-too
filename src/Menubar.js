7da86864463f5c3ea5ea0e6836dbacc315efdc97
Magee Mooney Caliguire <magee@flinkllc.com>
2016-08-05 09:30:31 -0700

    add custom Menubar material Appbar

---
+++ src/Menubar.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM, { render } from 'react-dom';
+import { AppBar } from 'react-toolbox/lib/app_bar'
+
+const Menubar = (props) => {
+  <App Bar {...props} />
+}
+
+export default MenuBar
