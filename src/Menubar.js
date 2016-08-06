ba49b14e2d4fe178e8636d97352541d04d7b4bd9
Magee Mooney Caliguire <magee@flinkllc.com>
2016-08-05 19:52:51 -0700

    eject create-react-app code

---
+++ src/Menubar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM, { render } from 'react-dom';
+import { AppBar } from 'react-toolbox/lib/app_bar'
+
+// const MenuBar = (props) => {
+//   <App Bar {...props} />
+// }
+
+const MenuBar = AppBar
+
+export default MenuBar
